721fa62c871a47000dd24258e90b04949c387c6c
patricknoni22 <okoloigwepatrick@gmail.com>
2026-04-22 15:59:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NewJean from "./components/NewJean";
+
+function App() {
+  return (
+    <div>
+      <NewJean />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewJean.jsx
@@ -0,0 +1,206 @@
+
+export default function NewJean() {
+
+
+
+      return (
+            <>
+
+                  <header>
+                        <div>
+                              <img src="./src/assets/njz.jpeg" alt="logo" />
+                        </div>
+                        <nav className="desk">
+                              <span>
+                                    <a href="https://www.instagram.com/newjeans_official/" target="_blank" rel="noopener noreferrer">Instagram</a>
+                              </span>
+                              <span>
+                                    <a href="https://www.tiktok.com/@newjeans_official" target="_blank" rel="noopener noreferrer"> TikTok </a>
+                              </span>
+
+                              <span>
+                                    <a href="https://www.facebook.com/official.newjeans" target="_blank">facebook</a>
+                              </span>
+                              <span>
+                                    <a href="https://www.youtube.com/@NewJeans_official" target="_blank" rel="noopener noreferrer">Youtube</a>
+                              </span>
+
+
+                        </nav>
+                        <nav className="mobile">
+                              <div className="moblink">
+                                    <span>
+                                          <a href="https://www.instagram.com/newjeans_official/" target="_blank" rel="noopener noreferrer">Instagram</a>
+                                    </span>
+                                    <span>
+                                          <a href="https://www.tiktok.com/@newjeans_official" target="_blank" rel="noopener noreferrer"> TikTok </a>
+                                    </span>
+
+                                    <span>
+                                          <a href="https://www.facebook.com/official.newjeans" target="_blank">facebook</a>
+                                    </span>
+                                    <span>
+                                          <a href="https://www.youtube.com/@NewJeans_official" target="_blank" rel="noopener noreferrer">Youtube</a>
+                                    </span>
+
+
+                              </div>
+                             
+
+                        </nav>
+
+                  </header>
+                  <main>
+                        <section>
+                              <div className="img1" >
+                                    <img src="./src\assets\newj.png" alt="bg-img" />
+                                    
+                              </div>
+                              <div className="mobimg">
+                                    <img src="./src\assets\new-2.png" alt="" />
+                              </div>
+                              <div className="txt1">
+                                    <p>NewJeans is a South Korean girl group formed by ADOR, a sub-label of Hybe. The group debuted with five members: Minji, Hanni, Danielle, Haerin, and Hyein. Formerly produced by Min Hee-jin, they are known for their girl next door image and musical stylings reminiscent of the 1990s and 2000s.</p>
+
+                              </div>
+
+                        </section>
+                        {/* section 2 */}
+                        <section>
+                              <h2>meet the members</h2>
+
+                              <div className="sec2">
+                                    <div className="sec2-img">
+                                          <img src="./src/assets/hanni.png" alt="Hanni" />
+                                          
+                                    </div>
+                                    <div className="sec2-txt">
+                                          <p>Hanni Pham (born October 6, 2004) is a Vietnamese-Australian singer, dancer, and member of the K-pop girl group NewJeans under ADOR. Born in Melbourne, Australia, she debuted with the group in July 2022 and is known for her vibrant vocals and performances. She is a key member of the five-piece group known for hits like "Hype Boy" and "Ditto</p>
+
+
+                                    </div>
+                              </div>
+                              {/* no 2 */}
+                              <div className="sec2">
+                                    <div className="sec2-img">
+                                          <img src="./src\assets\danielle.png" alt="Danielle" />
+                                        
+                                    </div>
+                                    <div className="sec2-txt">
+                                          <p>Danielle Marsh (born 2005) is an Australian-Korean singer and member of the K-pop group NewJeans. Known for her work in the group and as a, she continues a solo career while pursuing independent activities</p>
+
+
+                                    </div>
+                              </div>
+                              {/* no3 */}
+                              <div className="sec2">
+                                    <div className="sec2-img">
+                                          <img src="./src\assets\minji.png" alt="    Minji" />
+                                      
+                                    </div>
+                                    <div className="sec2-txt">
+                                          <p>Kim Min-ji (Korean: 김민지; born May 7, 2004), known mononymously as Minji,  debuted as a member of the South Korean girl group NewJeans. Minji gained immense popularity after her debut, ranking first in the girl group solo brand rankings for three consecutive months </p>
+
+
+                                    </div>
+                              </div>
+                              {/* no 4 */}
+                              <div className="sec2">
+                                    <div className="sec2-img">
+                                          <img src="./src\assets\haerin.png" alt="Haerin" />
+                                      
+                                    </div>
+                                    <div className="sec2-txt">
+                                          <p>Haerin (Kang Hae-rin) is a South Korean singer and member of the girl group NewJeans, born on May 15, 2006, in Seoul. Known for her cat-like visuals, calm personality, and sharp stage presence, she debuted under ADOR on July 22, 2022, and serves as a global ambassador for Dior </p>
+
+
+                                    </div>
+                              </div>
+                              {/* no 5 */}
+                              <div className="sec2">
+                                    <div className="sec2-img">
+                                          <img src="./src\assets\hyerin.png" alt="Hyein" />
+                                          
+                                    </div>
+                                    <div className="sec2-txt">
+                                          <p>Hyein (Lee Hye-in) is the youngest member (maknae) of the K-pop girl group NewJeans, born on April 21, 2008. She debuted with the group on July 22, 2022, under ADOR, and is known for her prior experience as a child model and member of children's groups U.SSO Girl and Play With Me Club. She is a global ambassador for Louis Vuitton. </p>
+
+
+                                    </div>
+                              </div>
+
+
+                        </section>
+                        <section>
+                              <h2>Top songs</h2>
+                              <div className="sec3">
+                                    <div>
+                                          <iframe  src="https://www.youtube.com/embed/Km71Rr9K-Bw?si=JWKgc5ivlPfiFfxQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                          <p>Ditto (frequently cited as their best track due to its nostalgic sound and iconic music videos)</p>
+
+                                    </div>
+                                    <div>
+                                          <iframe  src="https://www.youtube.com/embed/2Kff0U8w-aU?si=6qBNBvsHiiaOOjWz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                       
+                                          <p>OMG (Extremely popular with a highly viewed music video and notable choreography)</p>
+                                    </div>
+                                    <div>
+                                          <iframe  src="https://www.youtube.com/embed/ArmDp-zijuc?si=Mmp7FbkxyNPIPVqr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                         
+                                          <p>Super Shy (Known for its catchy and has high-energy choreography)</p> 
+                                    </div>
+                                    <div>
+                                          <iframe  src="https://www.youtube.com/embed/XCkpalH6Ppk?si=qv_a5w435QoLdzLx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                          
+                                          <p>ETA (Popular for its powerful and energetic "bouncy" beat)</p>
+                                    </div>
+                                    <div>
+                                          <iframe  src="https://www.youtube.com/embed/11cta61wi0g?si=i-svuu-U-FN6l_qN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                          <p>Hype Boy (Recognized as a massive hit with high member performance quality)</p>
+                                    </div>
+                                    <div>
+                                          <iframe  src="https://www.youtube.com/embed/js1CtxSY38I?si=lKousNcMyVQ_T0zJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                          <p>Attention (Widely regarded as a critically acclaimed debut song)</p>
+                                    </div>
+
+
+                              </div>
+                             
+                        </section>
+
+
+                  </main>
+                  <footer>
+
+                        <div className="footimg">
+                              <img src="./src\assets\njzz.png" alt="bunny" />
+                        </div>
+                        <div className="links">
+                              <ul>
+                                    <li>
+                                          <img src="./src\assets\Spotify Logo_.png" alt="" />
+                                          <a href="https://open.spotify.com/artist/6HvZYvR2fszIU3qGZWt6J2" target="_blank">spoitfy</a>
+                                    </li>
+                                    <li>
+                                          <img src="./src\assets\YTBMSC.png" alt="" />
+                                          <a href="https://music.youtube.com/channel/UCM7sJ88O9_BOC87V62v96_A" target="_blank">youtube music</a></li>   
+                                    <li>
+                                          <img src="./src\assets\Apple music logo.png" alt="" />
+                                          <a href="https://music.apple.com/artist/newjeans/1635469693" target="_blank">apple music</a>
+                                    </li>
+                                    
+                              </ul>
+                        </div>
+                  </footer>
+
+
+
+
+
+
+
+
+
+            </>
+      );
+}
